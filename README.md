454812bd2179d55c5dd0bb2621f6d83698e7aa66
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-10-28 10:15:49 +0100

    Convert to express middleware

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This package includes additional functionality for the AppDynamics Node.JS agent
 
 ## BTs for GraphQL
 
-Use the `appdynamics4graphql` context wrapper with apollo-server to have an out of the box detection of business transactions for your GraphQL application:
+Use the `appdynamics4graphql` middleware with apollo-server-express to have an out of the box detection of business transactions for your GraphQL application:
 
 ```javascript
 const appdynamics = require("appdynamics");
@@ -12,11 +12,13 @@ const { appdynamics4graphql } = require('appdynamics-nodejs-extras')
 ...
 // Set options for appdynamics4graphql
 const options = { ... }
-// Set up Apollo Server
+const app = express()
+app.use(json(), appdynamics4graphql(appdynamics, {debug: true}))
 const server = new ApolloServer({
-  context: appdynamics4graphql(context, appdynamics, options),
   ...
-});
+})
+server.applyMiddleware({ app })
+app.listen({ port: 4000 }, () => console.log(`app running`));
 ```
 
 With this your operation names will be reported as business transactions. If you don't provide an operation name the context wrapper will use the operation type as identifier (`query`, `mutation` or `subscription`) and call the BTs `unnamedQuery`, `unnamedMutation` and `unnamedSubscription`. If an error occurs or no name can be extracted, the default BT name is `IntrospectionQuery`.
@@ -24,5 +26,9 @@ With this your operation names will be reported as business transactions. If you
 You can reconfigure this default behaviour by setting the following options:
 
 * `inferWithField` (default: `false`): If set to true and if no operation name is provided, this will add the "root name" of your operation to the BT name. So instead of `unnamedQuery` you will get a BT named `query fieldname`.
-* `defaultBT` (default: `"IntrospectionQuery"`): Change the name of the default BT.
+* `defaultBt` (default: `"IntrospectionQuery"`): Change the name of the default BT.
 * `debug` (default: `false`): If set to true, there will be messages logged to `console`. As alternative you can provide a function that will be used to log those messages.
+* `addSnapshotData` (default: `true`): If set to true, add `operationName` and `operationType` to snapshot data.
+* `addAnalyticsData` (default: `true`): If set to true, add `opartionName` and `operationType` to analytics data.
+* `logQuery` (default: `false`): If set to true, the received GraphQL query will be logged into snapshot data (no analytics!).
+* exclusive (default: `true`): If set to true, a pre-existing business transaction will be terminated.
--- index.js
+++ index.js
@@ -1,46 +1,121 @@
-const appdynamics4graphql = (context, appdynamics, options) => {
+function extractOperation(string) {
+  // Remove all comments
+  const query = string.split(/(?:\r\n|\n|\r)/).filter(line => !line.startsWith('#')).join('\n');
+  // Seperate SelectionSet from OperationDefinition
+  const [ operationDefinition, selectionSet ] = query.replace(/(\s)+/gm,' ').split('{').map(e => e.trim());
 
-  const { inferWithField, defaultBT, debug } = Object.assign({inferWithField: false, defaultBT: 'IntrospectionQuery', debug: false }, options)
+  let operationType = 'query';
+  let operationName = '';
+  if(operationDefinition !== '') {
+    // debugFunction(operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g));
+    [operationType, operationName ] = operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g);
+  }
+  return {
+    operationName,
+    operationType
+  }
+}
 
-  debugFunction = typeof debug === 'function' ? debug : (debug === true ? console.log : () => {});
+const appdynamics4graphql = (appdynamics, options) => {
 
-  return async ({ req }) => {
+  const {
+    inferWithField,
+    defaultBt,
+    debug,
+    addSnapshotData,
+    addAnalyticsData,
+    logQuery,
+    exclusive
+   } = Object.assign({
+     inferWithField: false,
+     defaultBt: 'IntrospectionQuery',
+     debug: false,
+     addSnapshotData: true,
+     addAnalyticsData: true,
+     logQuery: false,
+     exclusive: true
+   }, options)
+
+  const debugFunction = typeof debug === 'function' ? debug : (debug === true ? console.log : () => {});
+
+  function collectData(tnx, key, value) {
+    if(addSnapshotData) {
+      debugFunction('Adding ' + key + ' and ' + value + ' to snapshot data');
+      tnx.addSnapshotData(key, value)
+    }
+    if(addAnalyticsData) {
+      debugFunction('Adding ' + key + ' and ' + value + ' to analytics data');
+      tnx.addAnalyticsData(key, value)
+    }
+  }
+
+  function startTransaction(btName, data, query, exception) {
+    debugFunction('BT is', btName);
+    const tnx = appdynamics.startTransaction(btName)
+    const myThreadId = tnx.time.threadId
+    if(tnx.agent.context.get('threadId') !== myThreadId) {
+      debugFunction('Setting threadId to', myThreadId)
+      tnx.agent.context.set('threadId', myThreadId)
+    }
+    if(data !== false) {
+      Object.keys(data).forEach(key => {
+        collectData(tnx, key, data[key])
+      })
+    }
+    if(query !== false && logQuery) {
+      // collectData(tnx, 'graphql-query', query)
+    }
+    if(exception !== false) {
+      tnx.addSnapshotData('appdynamics4graphql-error', exception.toString())
+    }
+    return tnx
+  }
+
+  return (req, res, next) => {
+    let tnx = null
+    if(exclusive) {
+      const oldTnx = appdynamics.getTransaction(req);
+      if(oldTnx) {
+        debugFunction('Ending existing business transaction')
+        oldTnx.end()
+      }
+    }
     try {
       // Make sure that a query is defined, otherwise this might not be a graphql request.
       // TODO: Check for graphql path?
       if(req.body && req.body.hasOwnProperty('query')) {
+        let btName = defaultBt
+        let { operationName, operationType } = extractOperation(req.body.query)
         if(req.body.operationName) {
-          console.log('BT is', req.body.operationName);
-          appdynamics.startTransaction(req.body.operationName);
+          operationName = req.body.operationName
+          btName = operationName
         } else {
-          // Remove all comments
-          const query = req.body.query.split(/(?:\r\n|\n|\r)/).filter(line => !line.startsWith('#')).join('\n');
-          // Seperate SelectionSet from OperationDefinition
-          const [ operationDefinition, selectionSet ] = query.replace(/(\s)+/gm,' ').split('{').map(e => e.trim());
-
-          let operationType = 'query';
-          let operationName = '';
-          if(operationDefinition !== '') {
-            debugFunction(operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g));
-            [operationType, operationName ] = operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g);
-          }
+          debugFunction(operationName, operationType)
           if(!operationName) {
             let field = inferWithField ? (' ' + selectionSet.match(/^([_A-Za-z][_0-9A-Za-z]*)/)[0]) : '';
             operationType = inferWithField ? operationType : 'unnamed' + operationType.charAt(0).toUpperCase() + operationType.slice(1);
-            appdynamics.startTransaction(operationType+field);
-            debugFunction('BT is', operationType+field);
+            btName = operationType+field;
           } else {
-            debugFunction('BT is', operationName);
-            appdynamics.startTransaction(operationName);
+            btName = operationName;
           }
         }
+        tnx = startTransaction(btName, { operationName, operationType }, req.body.query, false)
+      } else {
+        startTransaction(defaultBt, false, false)
+        debugFunction('No body or no query set')
       }
     } catch (e) {
-      // Do nothing, BT detection failed
-      debugFunction('BT is', defaultBT);
-      debugFunction(e);
+      debugFunction(e)
+      tnx = startTransaction(defaultBt, false, false, e)
     } finally {
-      return typeof context === 'function' ? await context({ req }) : null;
+      if (tnx) {
+        debugFunction('Attaching onResponse complete handler')
+        tnx.onResponseComplete = (req, res) => {
+          debugFunction('Terminating business transaction')
+          tnx.end()
+        }
+      }
+      next()
     }
   };
 };
