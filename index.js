e51c7f050dd0a6f6af78930570e62599eb73ace9
Severin Neumann <severin.neumann@altmuehlnet.de>
2019-10-29 10:14:28 +0100

    Add and apply eslint rules

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-function addEumCookie(debugFunction, transaction, agent, res, req) {
+function addEumCookie(debugFunction, transaction, res, req) {
   try {
-    var agent = transaction.agent
+    const agent = transaction.agent
 
-    var eumEnabled = (transaction.eumEnabled && !transaction.skip) || (agent.eum.enabled && agent.eum.enabledForTransaction(req));
+    const eumEnabled = (transaction.eumEnabled && !transaction.skip) || (agent.eum.enabled && agent.eum.enabledForTransaction(req))
 
     if (!transaction.corrHeader && eumEnabled) {
       agent.proxy.before(res, 'writeHead', function (obj) {
-        if(!transaction.isFinished) {
-          var eumCookie = self.agent.eum.newEumCookie(transaction, req, obj, false);
-          eumCookie.build();
+        if (!transaction.isFinished) {
+          var eumCookie = agent.eum.newEumCookie(transaction, req, obj, false)
+          eumCookie.build()
         }
-      });
+      })
     }
   } catch (e) {
     debugFunction(e)
@@ -19,15 +19,15 @@ function addEumCookie(debugFunction, transaction, agent, res, req) {
 
 function extractOperation(string) {
   // Remove all comments
-  const query = string.split(/(?:\r\n|\n|\r)/).filter(line => !line.startsWith('#')).join('\n');
+  const query = string.split(/(?:\r\n|\n|\r)/).filter(line => !line.startsWith('#')).join('\n')
   // Seperate SelectionSet from OperationDefinition
-  const [ operationDefinition, selectionSet ] = query.replace(/(\s)+/gm,' ').split('{').map(e => e.trim());
+  const [operationDefinition, selectionSet] = query.replace(/(\s)+/gm, ' ').split('{').map(e => e.trim())
 
-  let operationType = 'query';
-  let operationName = '';
-  if(operationDefinition !== '') {
+  let operationType = 'query'
+  let operationName = ''
+  if (operationDefinition !== '') {
     // debugFunction(operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g));
-    [operationType, operationName ] = operationDefinition.split(/[\(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g);
+    [operationType, operationName] = operationDefinition.split(/[(@]/)[0].match(/([_A-Za-z][_0-9A-Za-z]+)/g)
   }
   return {
     operationName,
@@ -37,35 +37,35 @@ function extractOperation(string) {
 }
 
 function infer(inferWithField, selectionSet, operationType) {
-  let field = inferWithField ? (' ' + selectionSet.match(/^([_A-Za-z][_0-9A-Za-z]*)/)[0]) : '';
-  operationType = inferWithField ? operationType : 'unnamed' + operationType.charAt(0).toUpperCase() + operationType.slice(1);
-  return operationType+field;
+  const field = inferWithField ? (' ' + selectionSet.match(/^([_A-Za-z][_0-9A-Za-z]*)/)[0]) : ''
+  operationType = inferWithField ? operationType : 'unnamed' + operationType.charAt(0).toUpperCase() + operationType.slice(1)
+  return operationType + field
 }
 
 function processResponse(debugFunction, collectData, chuncks, transaction) {
-  if(chuncks.length === 0) {
+  if (chuncks.length === 0) {
     debugFunction('Body is empty')
     return false
   }
   let body = ''
   try {
-    body = JSON.parse(typeof chuncks[0] === 'string' ? chuncks.join('') : Buffer.concat(chuncks).toString('utf8'));
-  } catch(e) {
+    body = JSON.parse(typeof chuncks[0] === 'string' ? chuncks.join('') : Buffer.concat(chuncks).toString('utf8'))
+  } catch (e) {
     debugFunction('Body is not json')
     return false
   }
-  if(body.errors && body.errors.length > 0) {
+  if (body.errors && body.errors.length > 0) {
     debugFunction('Errors detected')
     const error = body.errors[0]
-    let errorObject = new Error(body.errors[0].message)
-    if(error.extensions) {
+    const errorObject = new Error(body.errors[0].message)
+    if (error.extensions) {
       errorObject.code = error.extensions.exception.code
       errorObject.stack = error.extensions.exception.stacktrace.join('\n')
     }
     transaction.markError(errorObject)
-    if(body.errors.length > 1) {
-      body.errors.forEach((error,index) => {
-        collectData(transaction, 'error-'+index+'-message', error.message)
+    if (body.errors.length > 1) {
+      body.errors.forEach((error, index) => {
+        collectData(transaction, 'error-' + index + '-message', error.message)
       })
     }
     return true
@@ -73,7 +73,6 @@ function processResponse(debugFunction, collectData, chuncks, transaction) {
 }
 
 const appdynamics4graphql = (appdynamics, options) => {
-
   const {
     inferWithField,
     defaultBt,
@@ -86,51 +85,51 @@ const appdynamics4graphql = (appdynamics, options) => {
     exclusive,
     withEum,
     withResponseHook
-   } = Object.assign({
-     inferWithField: false,
-     defaultBt: 'unknownQuery',
-     debug: false,
-     addSnapshotData: true,
-     addAnalyticsData: true,
-     logQuery: false,
-     logRequestHeaders: [],
-     logResponseHeaders: [],
-     exclusive: true,
-     withEum: true,
-     withResponseHook: true
-   }, options)
+  } = Object.assign({
+    inferWithField: false,
+    defaultBt: 'unknownQuery',
+    debug: false,
+    addSnapshotData: true,
+    addAnalyticsData: true,
+    logQuery: false,
+    logRequestHeaders: [],
+    logResponseHeaders: [],
+    exclusive: true,
+    withEum: true,
+    withResponseHook: false
+  }, options)
 
-  const debugFunction = typeof debug === 'function' ? debug : (debug === true ? console.log : () => {});
+  const debugFunction = typeof debug === 'function' ? debug : (debug === true ? console.log : () => {})
 
   function collectData(transaction, key, value) {
-    if(addSnapshotData) {
-      debugFunction('Adding ' + key + ' and ' + value + ' to snapshot data');
+    if (addSnapshotData) {
+      debugFunction('Adding ' + key + ' and ' + value + ' to snapshot data')
       transaction.addSnapshotData(key, value)
     }
-    if(addAnalyticsData) {
-      debugFunction('Adding ' + key + ' and ' + value + ' to analytics data');
+    if (addAnalyticsData) {
+      debugFunction('Adding ' + key + ' and ' + value + ' to analytics data')
       transaction.addAnalyticsData(key, value)
     }
   }
 
   function startTransaction(btName, data, query, exception = false) {
-    debugFunction('BT is', btName);
+    debugFunction('BT is', btName)
     const transaction = appdynamics.startTransaction(btName)
 
     const myThreadId = transaction.time.threadId
-    if(transaction.agent.context.get('threadId') !== myThreadId) {
+    if (transaction.agent.context.get('threadId') !== myThreadId) {
       debugFunction('Setting threadId to', myThreadId)
       transaction.agent.context.set('threadId', myThreadId)
     }
-    if(data !== false) {
+    if (data !== false) {
       Object.keys(data).forEach(key => {
         collectData(transaction, key, data[key])
       })
     }
-    if(query !== false && logQuery) {
+    if (query !== false && logQuery) {
       collectData(transaction, 'graphql-query', query)
     }
-    if(exception !== false) {
+    if (exception !== false) {
       transaction.addSnapshotData('appdynamics4graphql-error', exception.toString())
     }
     return transaction
@@ -138,29 +137,29 @@ const appdynamics4graphql = (appdynamics, options) => {
 
   return (req, res, next) => {
     let transaction = null
-    if(false /* exclusive */) {
-      const oldTransaction = appdynamics.getTransaction(req);
-      if(oldTransaction) {
+    // eslint-disable-next-line no-constant-condition
+    if (false && exclusive) {
+      const oldTransaction = appdynamics.getTransaction(req)
+      if (oldTransaction) {
         debugFunction('Ending existing business transaction')
         oldTransaction.end()
       }
     }
     try {
       // Make sure that a query is defined, otherwise this might not be a graphql request.
-      // TODO: Check for graphql path?
-      if(req.body && req.body.hasOwnProperty('query')) {
+      if (req.body && Object.prototype.hasOwnProperty.call(req.body, 'query')) {
         let btName = defaultBt
         let { operationName, operationType, selectionSet } = extractOperation(req.body.query)
         debugFunction('Extracted operation', operationName, operationType, selectionSet)
-        if(req.body.operationName) {
+        if (req.body.operationName) {
           operationName = req.body.operationName
           btName = operationName
         } else {
           debugFunction(operationName, operationType)
-          if(!operationName) {
+          if (!operationName) {
             btName = infer(inferWithField, selectionSet, operationType)
           } else {
-            btName = operationName;
+            btName = operationName
           }
         }
         transaction = startTransaction(btName, { operationName, operationType }, req.body.query, false)
@@ -174,36 +173,32 @@ const appdynamics4graphql = (appdynamics, options) => {
     } finally {
       try {
         if (transaction) {
-          if(Array.isArray(logRequestHeaders)) {
+          if (Array.isArray(logRequestHeaders)) {
             logRequestHeaders.forEach(header => {
-              if(req.headers[header]) {
+              if (req.headers[header]) {
                 collectData(transaction, 'request-header-' + header, req.headers[header])
               }
             })
           }
-          if(withEum) {
+          if (withEum) {
             addEumCookie(debugFunction, transaction, res, req)
           }
           debugFunction('Attaching onResponse complete handler')
-          const once = (chunck) => {
-            console.log(chunck)
-            //res.removeEventListener(once)
-          }
 
-          let chuncks = []
-          if(withResponseHook) {
+          const chuncks = []
+          if (withResponseHook) {
             debugFunction('Hooking into res.write')
-            const oldWrite = res.write;
-            res.write = function(chunck) {
+            const oldWrite = res.write
+            res.write = function (chunck) {
               chuncks.push(chunck)
-              oldWrite.apply(res, arguments);
+              oldWrite.apply(res, arguments)
             }
           }
 
           res.on('finish', () => {
             try {
               let hasErrors = false
-              if(withResponseHook) {
+              if (withResponseHook) {
                 hasErrors = processResponse(debugFunction, collectData, chuncks, transaction)
               }
               if (!hasErrors && res.statusCode > 399 && res.statusCode < 600) {
@@ -211,26 +206,26 @@ const appdynamics4graphql = (appdynamics, options) => {
                 transaction.markError(new Error(), res.statusCode)
               }
               debugFunction('Terminating business transaction')
-              if(Array.isArray(logResponseHeaders)) {
+              if (Array.isArray(logResponseHeaders)) {
                 logResponseHeaders.forEach(header => {
-                  if(res.hasHeader(header)) {
+                  if (res.hasHeader(header)) {
                     collectData(transaction, 'response-header-' + header, res.getHeader(header))
                   }
                 })
               }
               transaction.end()
-            } catch(e) {
+            } catch (e) {
               debugFunction(e)
             }
           })
         }
-      } catch(e) {
+      } catch (e) {
         debugFunction(e)
       }
       next()
     }
-  };
-};
+  }
+}
 
 module.exports = {
   appdynamics4graphql
